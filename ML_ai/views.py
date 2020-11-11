a489ae7b468f889cb4a4ffb061c5f89448eea6e3
TateoKohara <68531009+TateoKohara@users.noreply.github.com>
2020-11-11 11:26:50 +0900

    メインアプリ実装

--- ML_ai/views.py
+++ ML_ai/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+    if request.method == "GET":
+        return render(
+            request,
+            "ML_ai/home.html"
+        )
+    else:    
+        title = request.POST["title"]
+        return render(
+            request,
+            "ML_ai/home.html",
+            {"title": title.split()}
+        )
+    
